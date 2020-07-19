eb78ee34da848e39ca24f4a27f96e9cb6d0c77b2
AlfredYuisGood <yde244910158@gmail.com>
2020-07-19 11:23:54 +1000

    Update 3

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -5,8 +5,7 @@ import CollectionPreview from '../collection-preview/collection-preview';
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className = 'collection-item'>
-        <div className = 'image' style = {{backgroundImage:`url(${imageUrl})`}}>
-        </div>
+        <div className = 'image' style = {{backgroundImage:`url(${imageUrl})`}} />
         <div className = 'collection-footer'>
             <span className = 'name'>{name}</span>
             <span className = 'price'>{price}</span>
